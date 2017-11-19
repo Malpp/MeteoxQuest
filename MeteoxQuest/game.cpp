4a72736b16d889a94ccdc9e367f55003c6a3eae5
Malpp <cpt.ortan@gmail.com>
2017-11-19 12:09:23 -0500

    Add project files.

---
+++ MeteoxQuest/game.cpp
@@ -0,0 +1,90 @@
+#include "stdafx.h"
+#include "Game.h"
+
+ResourceHandler Game::resource_handler_;
+
+void Game::input()
+{
+	while (window->pollEvent( event ))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			window->close();
+			break;
+
+		case sf::Event::KeyPressed:
+			switch (event.key.code)
+			{
+			case sf::Keyboard::Escape:
+				window->close();
+				break;
+			default:
+				break;
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+	}
+}
+
+void Game::update()
+{
+	float timeElapsed = clock.restart().asSeconds();
+
+	//Fps counter
+	frame_time_ += timeElapsed;
+	if (frame_time_ > 1)
+	{
+		std::cout << "FPS: " << frame_counter_ << "\n";
+		frame_time_ = 0;
+		frame_counter_ = 0;
+	}
+	frame_counter_++;
+
+	//Add new stuff below this
+}
+
+
+void Game::draw()
+{
+	window->clear();
+
+	//Draw code
+
+	window->display();
+}
+
+Game::Game()
+{
+	//Init window and view
+	window = new sf::RenderWindow( sf::VideoMode( WINDOW_WIDTH, WINDOW_HEIGHT ), "SFML", sf::Style::Close );
+	view.setSize( CAMARA_WIDTH, CAMERA_HEIGHT );
+	view.setCenter( GAME_WIDTH * 0.5f, GAME_HEIGHT * 0.5f );
+	window->setView( view );
+	clock.restart();
+	srand( time( nullptr ) );
+	rand();
+
+	resource_handler_.add_texture( "ben.png" );
+}
+
+Game::~Game()
+{
+	delete window;
+}
+
+void Game::run()
+{
+	window->setActive( false );
+
+	while (window->isOpen())
+	{
+		input();
+		update();
+		draw();
+	}
+}
---
+++ MeteoxQuest/game.h
@@ -0,0 +1,89 @@
+#pragma once
+#include "stdafx.h"
+#include "resourcehandler.h"
+
+class Game
+{
+
+public:
+	//Add new vars here
+
+private:
+	//Add new vars here
+
+//
+//TEMPLATE STUFF BELOW
+//
+
+public:
+	//Template variables
+	Game();
+	~Game();
+	void run();	
+	/// <summary>
+	/// The Game width
+	/// </summary>
+	static const int GAME_WIDTH = 1920;	
+	/// <summary>
+	/// The Game height
+	/// </summary>
+	static const int GAME_HEIGHT = 1080;	
+	/// <summary>
+	/// The camara width
+	/// </summary>
+	static const int CAMARA_WIDTH = 1920;	
+	/// <summary>
+	/// The camera height
+	/// </summary>
+	static const int CAMERA_HEIGHT = 1080;	
+	/// <summary>
+	/// The window width
+	/// </summary>
+	static const int WINDOW_WIDTH = 1280;	
+	/// <summary>
+	/// The window height
+	/// </summary>
+	static const int WINDOW_HEIGHT = 720;
+	static ResourceHandler resource_handler_;
+
+private:
+	//Template variables	
+	/// <summary>
+	/// The window
+	/// </summary>
+	sf::RenderWindow* window;	
+	/// <summary>
+	/// The event
+	/// </summary>
+	sf::Event event;	
+	/// <summary>
+	/// The clock
+	/// Used to time the frames
+	/// </summary>
+	sf::Clock clock;	
+	/// <summary>
+	/// The view
+	/// Uses the CAMERA_WIDTH and HEIGHT variables
+	/// </summary>
+	sf::View view;	
+	/// <summary>
+	/// Inputs.
+	/// </summary>
+	void input();	
+	/// <summary>
+	/// Updates the game.
+	/// </summary>
+	void update();	
+	/// <summary>
+	/// Draws the game.
+	/// </summary>
+	void draw();	
+	/// <summary>
+	/// The frame time
+	/// </summary>
+	float frame_time_ = 0;	
+	/// <summary>
+	/// The frame counter
+	/// </summary>
+	int frame_counter_ = 0;
+};
