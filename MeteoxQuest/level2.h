b7a6d8ef895aa596b4eb69b84d4603913856ee81
Malpp <cpt.ortan@gmail.com>
2017-12-17 14:23:17 -0500

    Added level 2,3,4 and backgrounds

---
+++ MeteoxQuest/level2.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "level_base.h"
+
+class Level2 : public LevelBase
+{
+public:
+	Level2(sf::RenderWindow* window);
+	void update(float delta_time) override;
+	static sf::Texture* texture_;
+};
